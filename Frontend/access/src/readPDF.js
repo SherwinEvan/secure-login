addaf3545dd0b0b90b5e15a682f61e5fe98d5724
Sherwin V Evan <sherwinvevan@outlook.com>
2023-06-01 19:23:49 +0530

    reader styled and used react-pdf

--- Frontend/access/src/readPDF.js
+++ Frontend/access/src/readPDF.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, LinearProgress } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import useRememberMe from "./service/rememberMe";
@@ -7,10 +8,24 @@ import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import { MuiFileInput } from "mui-file-input";
+import { Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
 
 export default function ReadPDF() {
   useRememberMe();
 
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const [pdfFile, setPDFFile] = useState(null);
   const [pdfUrl, setPdfUrl] = useState("");
   const [uploadProgress, setUploadProgress] = useState(0);
@@ -53,33 +68,78 @@ export default function ReadPDF() {
     reader.readAsDataURL(pdfFile);
   };
 
+  const onSubmit = (data) => {
+    console.log(data);
+    // Handle form submission
+  };
+
   return (
     <div className="flex flex-col h-screen justify-between">
       <NavBar />
       <div className="mb-auto">
-        <MuiFileInput
-          value={pdfFile}
-          placeholder="Select a PDF"
-          onChange={handleChange}
-        />
-        <Button
-          type="submit"
-          onClick={handleFileUpload}
-          variant="contained"
-          startIcon={<FileUploadIcon />}
-        >
-          Upload PDF
-        </Button>
+        <div className="text-2xl font-semibold text-center py-6">
+          Preview a PDF File
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex mx-5 py-3 space-x-10 items-center justify-center">
+            <div>
+              <Controller
+                name="pdfFile"
+                control={control}
+                rules={{
+                  required: "Select a PDF.",
+                  validate: (value) =>
+                    value[0]?.type === "application/pdf" ||
+                    "Only PDF files are allowed.",
+                }}
+                render={({ field }) => (
+                  <MuiFileInput
+                    {...field}
+                    onChange={handleChange}
+                    value={pdfFile}
+                    label="Upload PDF"
+                    placeholder="Select a file"
+                    error={!!errors.pdfFile}
+                    helperText={errors.pdfFile?.message}
+                    inputProps={{
+                      accept: ".pdf",
+                    }}
+                  />
+                )}
+              />
+            </div>
+            <div>
+              <Button
+                type="submit"
+                onClick={handleFileUpload}
+                variant="contained"
+                startIcon={<FileUploadIcon />}
+                size="large"
+              >
+                Upload PDF
+              </Button>
+            </div>
+          </div>
+          <div className="flex mx-5 italic justify-center">
+            Tip: Since, all your files are processed locally, smaller files are
+            processed faster.
+          </div>
+        </form>
+      </div>
+      <div className="p-10 m-5 shadow">
         {uploadProgress > 0 && (
           <LinearProgress variant="determinate" value={uploadProgress} />
         )}
-        {error && <p>{error}</p>}
+        {pdfUrl && (
+              <div style={{ height: '100vh' }}>
+              <Viewer
+                fileUrl={pdfUrl}
+                plugins={[defaultLayoutPluginInstance]}
+              />
+            </div>
+        
+        )}
       </div>
-      {pdfUrl && (
-        <div style={{ marginTop: "20px" }}>
-          <iframe src={pdfUrl} width="100%" height="600px" title="PDF Viewer" />
-        </div>
-      )}
       <Footer />
     </div>
   );
